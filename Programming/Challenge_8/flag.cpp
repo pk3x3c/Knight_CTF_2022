d35a7da807d185cbd7c8447d5745e3fb2785424b
Sahil Basia <53575624+Galaxy-Eater@users.noreply.github.com>
2022-01-22 14:45:06 +0530

    Update flag.cpp

--- Programming/Challenge_8/flag.cpp
+++ Programming/Challenge_8/flag.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {       
-        string flag="CFb5cp0rm1gK{1r4nT_m4}6";
+        string flag="CFb5cp0rm1gK{1r4nT_m4}6";      // this is the string we have to match ( i.e. the end result) so i reversed it to get the input flag
                
 	
 	for(int i=flag.length()-1; i>=0; --i)
